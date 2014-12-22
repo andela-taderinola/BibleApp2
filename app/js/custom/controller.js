7fecf4ac79da02d3a2412360dd3108449856a824
Timilehin Aderinola <timiderinola@gmail.com>
2014-12-22 14:07:52 +0100

    Major changes made

--- app/js/custom/controller.js
+++ app/js/custom/controller.js
@@ -1,11 +1,84 @@
 'use strict';
 
-var BibleApp = angular.module('BibleApp', []);
+var bibleApp = angular.module('bibleApp', ['bibleAppControllers']);
 
-BibleApp.controller('searchController', ['$scope', '$http', function (scope, http) {
-  //scope.searchResult = "";
-}]); //end searchController
+var bibleAppControllers = angular.module('bibleAppControllers', []);
 
-BibleApp.controller('openController', ['$scope', '$http', function (scope, http) {
-  //scope.response = "";
-}]); //end searchController
+// bibleAppControllers.controller('openController', ['$scope', function (scope) {
+//   scope.flipCover = function() {
+//     console.log("clicked");
+//     $("#homepageContainer").css("width", "-100em");
+//   };
+// }]); //end searchController
+
+bibleAppControllers.controller('bibleController', ['$scope', '$http', function (scope, http) {
+  scope.inputText = "";
+  scope.key = "517d06fdbe90e270534625197ed15845";
+
+  scope.open = function () {
+  $(".navbar").css({
+    "transition": "0.5s ease-in-out .5s",
+    "-webkit-transition": "0.5s ease-in-out .5s",
+    "-moz-transition": "0.5s ease-in-out .5s",
+    "-o-transition": "0.5s ease-in-out .5s",
+    "position": "absolute",
+    "width": "100%",
+    "top": "-3em",
+    "height": "4em",
+    "padding-top": "0.1em",
+    "text-align": "center"
+  });
+
+  $( "div" ).hover(
+  function() {
+    $(".navbar").css({"top": "0"});
+  }, function() {
+    $(".navbar").css({"top": "-3em"});
+  }
+);
+    scope.searchBible();
+  }
+  scope.searchBible = function() {
+  
+    if(!scope.inputText.trim()) {
+      scope.info = "Input field must not be empty";
+      return;
+    }
+    scope.info = "Please wait...";
+    http({url: 'http://api.biblia.com/v1/bible/search/DARBY.js', method: 'GET', params: {query: scope.inputText,
+        sort: "passage", key: scope.key}}).success(function (data) {
+          scope.info = "Done";
+    if(data.hitCount) {
+      scope.info = "";
+      scope.searchResponse = data;
+      scope.searchResult = scope.searchResponse.results;
+      $("#searchListContainer").fadeIn(1500);
+    } else {
+      scope.info = "Checking in Bible...";
+      scope.openBible();
+    }
+  }).error(function (status) {
+      scope.failEvent(scope.errorStatus);
+  });  
+} //end of function openBible
+
+scope.openBible = function (status){
+    http({url: 'http://api.biblia.com/v1/bible/content/DARBY.html.json', method: 'GET', params: {passage: scope.inputText,
+        style: "fullyFormattedWithFootnotes",
+        key: scope.key}}).success(function (data) {
+          scope.info = "Done";
+      scope.response = data;
+      scope.verse = scope.response.text;
+      scope.display = '<div id="content">' + scope.verse + '</div>';
+      $("#container").html(scope.display);
+      $("#container").fadeIn(1500);
+      scope.info = "";
+    }).error(function (data) {
+      scope.failEvent(scope.errorStatus);
+    }); 
+  }
+
+  scope.failEvent = function(error) {
+      scope.info = "Sorry, didn't get that. Please check your input or internet connection.";
+  }
+}]);
--- test/unit/controllerSpec.js
+++ test/unit/controllerSpec.js
@@ -10,22 +10,23 @@ describe('BibleApp Controllers', function() {
     beforeEach(module('BibleApp'));
 
     beforeEach(inject(function ($httpBackend, $rootScope, $controller) {
-        scope = $rootScope.$new();
-        httpGet = $httpBackend;
-        controller = $controller('searchController', {$scope:scope});
-        httpGet.expectGET('../../app/data/searchJacob.json').respond({text: "Jacob"});
+      scope = $rootScope.$new();
+      httpGet = $httpBackend;
+      controller = $controller('searchController', {$scope:scope});
+      httpGet.expectGET('app/data/searchJacob.json').respond({text: "Jacob"});
     })); //end beforeEach inject
 
     it('should return an object', function() {
-      expect(typeof scope.searchResult).toBeUndefined();
+      expect(scope.searchResult).toBeUndefined();
       httpGet.flush();
-      expect(typeof scope.searchResult).toEqual('Object');
+      expect(typeof scope.searchResult).toEqual('object');
     });  //end of 'it should return an object'
 
     it('should return an object with property text: "Jacob"', function() {
       httpGet.flush();
       expect(scope.searchResult.text).toEqual('Jacob');
     }); //end of second it
+    
   }); // end searchController
 
   describe('openController', function() {
@@ -37,21 +38,22 @@ describe('BibleApp Controllers', function() {
     beforeEach(module('BibleApp'));
 
     beforeEach(inject(function ($httpBackend, $rootScope, $controller) {
-        scope = $rootScope.$new();
-        httpGet = $httpBackend;
-        controller = $controller('searchController', {$scope:scope});
-        httpGet.expectGET('../../app/data/john17.json').respond({passage: "John 17"});
+      scope = $rootScope.$new();
+      httpGet = $httpBackend;
+      controller = $controller('openController', {$scope:scope});
+      httpGet.expectGET('app/data/john17.json').respond({passage: "John 17"});
     })); //end beforeEach inject
 
-    it('should return an object', function() {
-      expect(typeof scope.response).toBeUndefined();
+    it('should return JSON data', function() {
+      expect(scope.response).toBeUndefined();
       httpGet.flush();
-      expect(typeof scope.searchResult).toEqual('Object');
+      expect(scope.response).toBeDefined();
     });  //end of 'it should return an object'
 
     it('should return an object with property passage: "John 17"', function() {
       httpGet.flush();
       expect(scope.response.passage).toEqual('John 17');
     }); //end of second it
+
   }); // end openController
 }); //end BibleApp Controllers
