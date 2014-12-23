553e64686729a532223ac0f8886596878cc1560b
Timilehin Aderinola <timiderinola@gmail.com>
2014-12-23 11:27:25 +0100

    Tidying up

--- js/custom/controller.js
+++ js/custom/controller.js
@@ -9,27 +9,6 @@ bibleAppControllers.controller('bibleController', ['$scope', '$http', function (
   scope.key = "517d06fdbe90e270534625197ed15845";
 
   scope.open = function () {
-  $(".navbar").css({
-    "transition": "0.5s ease-in-out .5s",
-    "-webkit-transition": "0.5s ease-in-out .5s",
-    "-moz-transition": "0.5s ease-in-out .5s",
-    "-o-transition": "0.5s ease-in-out .5s",
-    "position": "absolute",
-    "width": "100%",
-    "top": "-3em",
-    "height": "4em",
-    "padding-top": "0.1em",
-    "z-index": "1",
-    "text-align": "center"
-  });
-
-  $( "div" ).hover(
-  function() {
-    $(".navbar").css({"top": "0"});
-  }, function() {
-    $(".navbar").css({"top": "-3em"});
-  }
-);
     scope.searchBible();
   }
   scope.searchBible = function() {
@@ -42,17 +21,20 @@ bibleAppControllers.controller('bibleController', ['$scope', '$http', function (
     http({url: 'http://api.biblia.com/v1/bible/search/DARBY.js', method: 'GET', params: {query: scope.inputText,
         sort: "passage", key: scope.key}}).success(function (data) {
           scope.info = "Done";
-    if(data.hitCount) {
+    if(data.hitCount > 0) {
       scope.info = "";
       scope.searchResponse = data;
       scope.searchResult = scope.searchResponse.results;
+      scope.searchItem = scope.inputText;
       $("#searchListContainer").fadeIn(1500);
+      $(".toReveal").fadeIn(1000);
+      scope.rollUp();
     } else {
       scope.info = "Checking in Bible...";
       scope.openBible();
     }
-  }).error(function (status) {
-      scope.failEvent(scope.errorStatus);
+  }).error(function (data, status) {
+      scope.failEvent(status);
   });  
 } //end of function openBible
 
@@ -67,12 +49,58 @@ scope.openBible = function (status){
       $("#container").html(scope.display);
       $("#container").fadeIn(1500);
       scope.info = "";
-    }).error(function (data) {
-      scope.failEvent(scope.errorStatus);
-    }); 
+      scope.rollUp();
+    }).error(function (data, status) {
+      scope.failEvent(status);
+      return;
+    });
+    scope.rollUp(); 
   }
 
-  scope.failEvent = function(error) {
-      scope.info = "Sorry, didn't get that. Please check your input or internet connection.";
+  scope.rollUp = function() {
+    $(".navbar").css({
+    "transition": "0.5s ease-in-out .5s",
+    "-webkit-transition": "0.5s ease-in-out .5s",
+    "-moz-transition": "0.5s ease-in-out .5s",
+    "-o-transition": "0.5s ease-in-out .5s",
+    "position": "absolute",
+    "width": "100%",
+    "top": "-3em",
+    "height": "4em",
+    "padding-top": "0.1em",
+    "z-index": "1",
+    "text-align": "center"
+  });
+
+  $( "div" ).hover(
+    function() {
+      $(".navbar").css({"top": "0"});
+    }, function() {
+      $(".navbar").css({"top": "-3em"});
+    }
+  );
+  }
+
+  scope.failEvent = function(status) {
+      if(status === 0) {
+      scope.info = "There seems to be a problem with your internet connection.";
+    }
+    else if (status === 404) {
+      scope.info = "Sorry, didn't get that.";
+    }
+    else if (status === 500) {
+      scope.info = "Server error. Please try again.";
+    }
+    else {
+      scope.info = "Looks like there's a little hiccup: (" + status + ")";
+    }
+    $(".navbar").css("top", "0");
+    $( "div" ).hover(
+    function() {
+      $(".navbar").css({"top": "0"});
+    }, function() {
+      $(".navbar").css({"top": "0"});
+    }
+  );
   }
 }]);
